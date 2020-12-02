6e0747877c5960a76ce68b32f3d5cce9c14865e8
Matthew White <mwhite@plex.com>
2020-12-02 11:47:31 -0500

    Part 2 tested

--- src/day2/mod.rs
+++ src/day2/mod.rs
@@ -5,6 +5,11 @@ pub fn get_valid_count(input: Vec<PasswordPolicy>) -> usize
   input.iter().map(|x| x.validate()).filter(|x| *x).count()
 }
 
+pub fn get_valid_count_part2(input: Vec<PasswordPolicy>) -> usize
+{
+  input.iter().map(|x| x.validate2()).filter(|x| *x).count()
+}
+
 pub struct PasswordPolicy {
   range: std::ops::RangeInclusive<usize>,
   c: char,
@@ -38,6 +43,15 @@ impl PasswordPolicy {
     let hits = self.password.chars().filter(|&x| x == self.c).count();
     self.range.contains(&hits)
   }
+
+  pub fn validate2(&self) -> bool {
+    let cs : Vec<char> = self.password.chars().collect();
+    let first = cs[*self.range.start() - 1];
+    let second : char = cs[*self.range.end() - 1];
+
+    (first == self.c && second != self.c) ||
+    (first != self.c && second == self.c)
+  }
 }
 
 #[cfg(test)]
@@ -48,5 +62,11 @@ mod tests {
   fn day2_part1() {
     let example = vec!["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
     assert_eq!(2, get_valid_count(example.iter().map(|x| PasswordPolicy::from_str(x).unwrap()).collect()));
+  }
+
+  #[test]
+  fn day2_part2() {
+    let example = vec!["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
+    assert_eq!(1, get_valid_count_part2(example.iter().map(|x| PasswordPolicy::from_str(x).unwrap()).collect()));
   }
 }
