2d84d340cc73e79d9850825bd4b2036cceaa04e7
Matthew White <mwhite@plex.com>
2020-12-02 11:48:17 -0500

    Part 2 successful

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ fn main()-> Result<(), Box<dyn Error>> {
 
   let input : Vec<day2::PasswordPolicy> = helpers::inputhandling::parse_input_per_line(2, |s| s.parse::<day2::PasswordPolicy>().map_err(|e| e.into()))?;
 
-  dbg!(day2::get_valid_count(input));
-  //dbg!(day1::find_2020_and_multiply_part2(input.clone()));
+  //dbg!(day2::get_valid_count(input));
+  dbg!(day2::get_valid_count_part2(input));
 
   Ok(())
 }
