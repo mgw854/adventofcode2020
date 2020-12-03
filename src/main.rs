099b9747f472f543990a0875e07dba74103ceb65
Matthew White <mwhite@plex.com>
2020-12-03 00:22:58 -0500

    Part 2, cakewalk

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,13 @@ mod helpers;
 fn main()-> Result<(), Box<dyn Error>> { 
   let input = helpers::inputhandling::get_string(3).unwrap();
     
-  dbg!(day3::hit_trees(input.lines().collect(), 3, 1));
+  let attempt1 = day3::hit_trees(input.lines().collect(), 1, 1);
+  let attempt2 = day3::hit_trees(input.lines().collect(), 3, 1);
+  let attempt3 = day3::hit_trees(input.lines().collect(), 5, 1);
+  let attempt4 = day3::hit_trees(input.lines().collect(), 7, 1);
+  let attempt5 = day3::hit_trees(input.lines().collect(), 1, 2);
+
+  dbg!(attempt1 * attempt2 * attempt3 * attempt4 * attempt5);
   //dbg!(day2::get_valid_count_part2(input));
 
   Ok(())
