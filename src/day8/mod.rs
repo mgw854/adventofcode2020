8120dd5e1ce47ccf08d860c3551a30e3426dfd0f
Matthew White <mwhite@plex.com>
2020-12-08 22:18:15 -0500

    Part 2 was suprisingly easy

--- src/day8/mod.rs
+++ src/day8/mod.rs
@@ -9,7 +9,7 @@ pub fn execute(program: Vec<AsmInstruction>) -> i64 {
     visited.insert(instruction_pointer, ());
 
     instruction_pointer = (instruction_pointer as i64 + match program[instruction_pointer] {
-      AsmInstruction::NoOp => 1,
+      AsmInstruction::NoOp(_) => 1,
       AsmInstruction::Accumulate(x) => { accum += x; 1 },
       AsmInstruction::Jump(x) => x
     }) as usize;
@@ -18,9 +18,53 @@ pub fn execute(program: Vec<AsmInstruction>) -> i64 {
   accum
 }
 
-#[derive(Debug, PartialEq, Eq)]
+pub fn execute_mutate(program: Vec<AsmInstruction>) -> i64 {
+  let mut instruction_pointer = 0;
+
+  loop
+  {   
+    let mut program2 = program.clone();
+    // find the first instruction that is a jmp or nop, and flip it
+    for ip in instruction_pointer..program2.len() {
+      match program2[ip] {
+        AsmInstruction::Accumulate(_) => {},
+        AsmInstruction::NoOp(x) => { instruction_pointer = ip + 1; program2[ip] = AsmInstruction::Jump(x); break; },
+        AsmInstruction::Jump(x) => { instruction_pointer = ip + 1; program2[ip] = AsmInstruction::NoOp(x); break; }
+      }
+    }
+    
+    match execute_mutate_internal(program2) {
+      Ok(i) => return i,
+      _ => {}
+    }
+  }
+}
+
+fn execute_mutate_internal(program: Vec<AsmInstruction>) -> Result<i64, ()> {
+  let mut instruction_pointer = 0;
+  let mut accum = 0;
+  let mut visited : HashMap<usize, ()> = HashMap::new();
+
+  while !visited.contains_key(&instruction_pointer) && instruction_pointer < program.len() {
+    visited.insert(instruction_pointer, ());
+
+    instruction_pointer = (instruction_pointer as i64 + match program[instruction_pointer] {
+      AsmInstruction::NoOp(_) => 1,
+      AsmInstruction::Accumulate(x) => { accum += x; 1 },
+      AsmInstruction::Jump(x) => x
+    }) as usize;
+  }
+
+  if instruction_pointer >= program.len() {
+    return Ok(accum);
+  }
+
+  Err(())
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum AsmInstruction {
-  NoOp,
+  NoOp(i64),
   Accumulate(i64),
   Jump(i64)
 }
@@ -31,7 +75,7 @@ impl AsmInstruction {
     let arg : i64 = split[1].parse().unwrap();
 
     match split[0] {
-      "nop" => AsmInstruction::NoOp,
+      "nop" => AsmInstruction::NoOp(arg),
       "acc" => AsmInstruction::Accumulate(arg),
       "jmp" => AsmInstruction::Jump(arg),
       _ => panic!()
@@ -45,7 +89,7 @@ mod tests {
 
   #[test]
   fn parse_nop() {
-    assert_eq!(AsmInstruction::NoOp, AsmInstruction::parse("nop +0"));
+    assert_eq!(AsmInstruction::NoOp(0), AsmInstruction::parse("nop +0"));
   }
 
   #[test]
@@ -71,5 +115,20 @@ jmp -4
 acc +6";
 
     assert_eq!(5, execute(input.lines().map(|x| AsmInstruction::parse(x)).collect()));
+  }
+
+  #[test]
+  fn day8_part2() {
+    let input = "nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6";
+
+    assert_eq!(8, execute_mutate(input.lines().map(|x| AsmInstruction::parse(x)).collect()));
   }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ mod helpers;
 fn main()-> Result<(), Box<dyn Error>> { 
   let input = helpers::inputhandling::parse_input_per_line(8, |l| Ok(day8::AsmInstruction::parse(l))).unwrap();
 
-  dbg!(day8::execute(input));
+  dbg!(day8::execute_mutate(input));
   Ok(())
 }
