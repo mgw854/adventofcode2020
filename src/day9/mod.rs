58e048bf50083d2b5e803c1d1816d9e0311bbc55
Matthew White <mwhite@plex.com>
2020-12-09 22:37:25 -0500

    Part 1 done

--- src/day9/mod.rs
+++ src/day9/mod.rs
@@ -1,11 +1,11 @@
 use itertools::Itertools;
 
-pub fn find_invalid_number(input: Vec<i32>, preamble: usize) -> i32
+pub fn find_invalid_number(input: Vec<u64>, preamble: usize) -> u64
 {
   let mut start = preamble;
 
   while start < input.len() {
-    let preamble_sums : Vec<i32> = input[(start-preamble)..start].iter().map(|x| *x).collect::<Vec<i32>>().iter().combinations(2).map(|x| x[0] + x[1]).collect();
+    let preamble_sums : Vec<u64> = input[(start-preamble)..start].iter().map(|x| *x).collect::<Vec<u64>>().iter().combinations(2).map(|x| x[0] + x[1]).collect();
 
     if !preamble_sums.contains(&input[start]) {
       return input[start];
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ mod day9;
 mod helpers;
 
 fn main()-> Result<(), Box<dyn Error>> { 
-  //let input = helpers::inputhandling::parse_input_per_line(8, |l| Ok(day8::AsmInstruction::parse(l))).unwrap();
+  let input = helpers::inputhandling::parse_input_per_line(9, |l| Ok(l.parse::<u64>().unwrap())).unwrap();
 
-  //dbg!(day8::execute_mutate(input));
+  dbg!(day9::find_invalid_number(input, 25));
   Ok(())
 }
